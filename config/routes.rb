098557753bcfe65e3426ebb466717e858ba27027
PAOLO DE DIOS <dedios.paolo.g@gmail.com>
2025-06-21 10:30:23 +0800

    Getting started with rails using guide. CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "articles#index"
+
+  resources :articles
+  # resources :articles, only: [:index, :show, :new]
 end
